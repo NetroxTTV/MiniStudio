87ff48b464889dd1f41851270d7653fc9b243a40
LoganDANgtech <147987555+LoganDANgtech@users.noreply.github.com>
2024-04-03 16:50:40 +0200

    yooooooooooooooooooooooooooow

--- game.py
+++ game.py
@@ -1,90 +1,198 @@
 import pygame
 import time
+from pygame.locals import *
+import sys
+import math
+import random
 #from sol import Sol
 pygame.init()
 
-class BaseWindow:
+class BaseWindow(pygame.sprite.Sprite): # PAS TOUCHE
     def __init__(self) -> None:
+        super().__init__()
         self.wid = 1920
         self.hei = 1080
         self.fps = 120
+        self.ACC = 1
+        self.FRIC = -0.14
+        self.FramePerSec = pygame.time.Clock()
+        self.vec = pygame.math.Vector2 
 
-class Player:
-    def __init__(self) -> None:
-        self.x = (BaseWindow().wid)//2
-        self.y = (BaseWindow().hei)//2
+class Player(pygame.sprite.Sprite): # PAS TOUCHE
+    def __init__(self):
+        super().__init__() 
         self.speed = 1
         self.life = 100
         self.atk = 10
         
-    def gravity(self):
-        self.y += 3.2 
+        self.surf = pygame.Surface((80, 80))
+        self.rect = self.surf.get_rect()
+   
+        self.pos = BaseWindow().vec((1920/2, 1080/2))
+        self.vel = BaseWindow().vec(0,0)
+        self.acc = BaseWindow().vec(0,0)
 
-    def player(self, x, y):
-        gameDisplay.blit(playerIMG, (x, y))
+        self.fric = BaseWindow().FRIC
 
+    def move(self):
+        self.acc = BaseWindow().vec(0,0.5)
+    
+        pressed_keys = pygame.key.get_pressed()            
+        if pressed_keys[K_s]:
+            self.slide()
+        else:
+            if pressed_keys[K_q]:
+                self.acc.x = -BaseWindow().ACC
+                self.direction = 0
+            if pressed_keys[K_d]:
+                self.acc.x = BaseWindow().ACC
+                self.direction = 1
+             
+        self.acc.x += self.vel.x * self.fric
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc
+         
+        if self.pos.x > BaseWindow().wid:
+            self.pos.x = 0
+        if self.pos.x < 0:
+            self.pos.x = BaseWindow().wid
+            
+        self.rect.midbottom = self.pos
+        self.fric = BaseWindow().FRIC
+
+    def update(self):
+        hits = pygame.sprite.spritecollide(P1 ,platforms, False)
+        if P1.vel.y > 0:        
+            if hits:
+                self.vel.y = 0
+                self.pos.y = hits[0].rect.top + 1
+    	
+    def jump(self):
+        hits = pygame.sprite.spritecollide(self, platforms, False)
+        if hits:
+            self.vel.y = -15
+    
+    def dash(self):
+        hits = pygame.sprite.spritecollide(P1 ,platforms, False)
+        if hits and -10 < self.vel.x < 10:
+            self.vel.x *= 5
+    
+    def slide(self):
+        hits = pygame.sprite.spritecollide(P1 ,platforms, False)
+        if hits:
+            self.fric = -0.0004
+
+class snowball:
+    def __init__(self, x, y, targetx, targety):
+        self.speed = 20
+    
+        self.surf = pygame.Surface((20,20))
+        self.rect = self.surf.get_rect()
+
+        self.rect.midbottom = P1.rect.midbottom
+        self.point = pygame.mouse.get_pos()
+
+        self.angle = math.atan2(targety-y, targetx-x) 
+
+        self.dir = BaseWindow().vec(math.cos(self.angle)*self.speed,math.sin(self.angle)*self.speed)
+        self.acc = BaseWindow().vec(0,0)
+        self.vel = self.dir
+        self.pos = BaseWindow().vec((x, y))
+
+    def move(self):
+        self.acc = BaseWindow().vec(0,0.3)
+        
+
+        self.acc.x += self.vel.x * -0.001
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc
+         
+        self.rect.center = self.pos
+
+class platform(pygame.sprite.Sprite): # PAS TOUCHE
+    def __init__(self):
+        super().__init__()
+        self.surf = pygame.Surface((BaseWindow().wid, 20))
+        self.surf.fill((100,100,100))
+        self.rect = self.surf.get_rect(center = (BaseWindow().wid/2, BaseWindow().hei - 10))
+ 
 def setup_imgs(window_width, window_height):
     # background image
     background = pygame.transform.scale(pygame.image.load(r'bg2.jpg'), (window_width, window_height))
 
     # player image + rotate
-    img = pygame.transform.scale(pygame.image.load(r'pik.png'), (100, 100))
+    img = pygame.transform.scale(pygame.image.load(r'pik.png'), (80, 80))
     playerI = pygame.transform.rotate(img, 0)
 
-    return background, playerI
+    # snowball image
+    snowballimg = pygame.transform.scale(pygame.image.load(r'pik.png'), (20, 20))
 
-def gd(window_width, window_height):
+    return background, playerI, snowballimg
+
+def gd(window_width, window_height): # PAS TOUCHE
     gd = pygame.display.set_mode((window_width,window_height))
-    pygame.display.set_caption('Space Crashers')
+    pygame.display.set_caption('PIKMIN')
     return gd
-
-def play(gameDisplay, playerX, playerY):
+    
+def play(gameDisplay):
     
     clock = pygame.time.Clock()
     running = True
-    dt = 0.01
-    a = 0
+    s = []
 
     while running:
 
         gameDisplay.fill((0, 0, 0))
         gameDisplay.blit(bg, (0, 0))
-        Player().player(playerX, playerY)
         
         for event in pygame.event.get():
 
 ################### CLICK CHECK ################### 
             
-            point = pygame.mouse.get_pos()
-
-
             if pygame.mouse.get_pressed()[0]:
-                a=a+1  
-                time.sleep(0.1)
-                pygame.draw.circle(bg, "red", point, 20, width=0)
-                print("yes", a)
-
-
+                x,y = pygame.mouse.get_pos()
+                s.append(snowball(P1.rect.midbottom[0] - 25, P1.rect.midbottom[1] -25,  x,y ))
+                snowball(P1.rect.midbottom[0] - 25, P1.rect.midbottom[1] - 25,  x,y)
 ################### PLAYER MOVEMENT ################### 
             
             keys = pygame.key.get_pressed()
             if keys[pygame.K_o]:
                 pygame.QUIT
                 running = False
-            if keys[pygame.K_z]:
-                    playerY -= 500 * dt
-            if keys[pygame.K_s]:
-                    playerY += 500 * dt
-            if keys[pygame.K_q]:
-                    playerX -= 500 * dt
-            if keys[pygame.K_d]:
-                    playerX += 500 * dt
+            if keys[pygame.K_SPACE]:
+                P1.jump()
+            if keys[pygame.K_LSHIFT]:
+                P1.dash()
+
+        gameDisplay.fill((0,0,0))
+        P1.move()
+        P1.update()
+
+        gameDisplay.blit(bg, (0, 0))
+        gameDisplay.blit(playerIMG, P1.rect)
+        gameDisplay.blit(PT1.surf, PT1.rect)
+        for entity in s:
+
+            gameDisplay.blit(snowballimg, entity.rect)
+            entity.move()
+
         pygame.display.update() 
         clock.tick(BaseWindow().fps)
+
     #####################
 
-bg, playerIMG = setup_imgs(BaseWindow().wid, BaseWindow().hei)
+PT1 = platform()
+P1 = Player()
+
+all_sprites = pygame.sprite.Group()
+all_sprites.add(PT1)
+all_sprites.add(P1)
+
+platforms = pygame.sprite.Group()
+platforms.add(PT1)
+
+bg, playerIMG, snowballimg = setup_imgs(BaseWindow().wid, BaseWindow().hei)
 gameDisplay = gd(BaseWindow().wid, BaseWindow().hei)
-play(gameDisplay, Player().x, Player().y)
+play(gameDisplay)
 
 pygame.quit()
